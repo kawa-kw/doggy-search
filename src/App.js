143404f1f33cca34f64f1f55e0e84e50fdcbecbb
kawa <katarzyna.walenko@21s.pl>
2018-10-01 23:48:09 +0200

    ^1: add sass to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo-doggy.svg';
-import './App.css';
+import './static/styles/App.css';
 
 class App extends Component {
   render() {
