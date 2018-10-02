6e8ed181c0c87bbe769b1768aea787e7ca90d72c
kawa <katarzyna.walenko@21s.pl>
2018-10-03 00:36:53 +0200

    #3: get dogs pictures from flickr

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,41 @@ import React, { Component } from 'react';
 import logo from '../static/img/logo-doggy.svg';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {pictures: []};
+    }
+
+    componentDidMount() {
+        fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c22d48140f5ca304816ef08963e4e4fe&tags=dogs&page=1&format=json&nojsoncallback=1')
+            .then((response) => response.json())
+            .then((results) => {
+                let picArray = results.photos.photo.map((pic) => {
+                    const srcPath =
+                    `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`
+                    return (
+                        <img alt='dog' src={srcPath} />
+                    )
+                })
+                this.setState({ pictures: picArray })
+            })
+    }
+
+
+    render() {
+        console.log('to:', this.state.pictures.length)
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <div className="App-intro">
+              {this.state.pictures}
+            </div>
+
+          </div>
+        );
+    }
 }
 
 export default App;
